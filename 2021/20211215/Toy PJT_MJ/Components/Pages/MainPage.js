9b489890793a8ca4fed410a4be1ced173842d6b9
MinjunShin <mj.irin1260@gmail.com>
2021-12-15 19:31:03 +0900

    Toy PJT : Minjun

---
+++ 2021/20211215/Toy PJT_MJ/Components/Pages/MainPage.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export default function MainPage () {
+
+  const MainPage = styled.div`
+    border: 2px solid gray;
+    flex : 9 0 0;
+    height : 100vh;
+  `
+
+  return (
+    <MainPage>
+      This is Describe page
+    </MainPage>
+  )
+}
