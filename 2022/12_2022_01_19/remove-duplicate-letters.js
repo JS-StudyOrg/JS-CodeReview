20976b006f9d43c66507493ae37b7c1d5c4706dc
sora9z <sssssssorasss@gmail.com>
2022-01-15 23:36:24 +0900

    11_2022-01-19 Study dircoty & 문제 추가

---
+++ 2022/12_2022_01_19/remove-duplicate-letters.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/remove-duplicate-letters/
+
+// 중복된 문자를 제외하고 사전식 순서로 나열하는 문제
+
+/*
+Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+
+EX1
+Input: s = "bcabc"
+Output: "abc"
+
+EX2
+Input: s = "cbacdcbc"
+Output: "acdb"
+
+Constraints:
+1 <= s.length <= 104
+s consists of lowercase English letters.
+
+
+! HINT : Stack
+
+*/
