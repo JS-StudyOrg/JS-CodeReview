eb46bc55eaebf1d37bde33d51f635499e8fcf67b
sora9z <sssssssorasss@gmail.com>
2022-01-15 23:39:14 +0900

    11_2022-01-19 문제에 설명 추가

--- 2022/12_2022_01_19/remove-duplicate-letters.js
+++ 2022/12_2022_01_19/remove-duplicate-letters.js
@@ -3,6 +3,10 @@
 // 중복된 문자를 제외하고 사전식 순서로 나열하는 문제
 
 /*
+? 자료구조를 알고리즘에 적용하는 연습을 하고싶어 문제를 골아보았습니다.  
+? ,queue stack과 같은 기본적인 자료구조부터 시작하여 관련 문제를 풀면서
+? 자료구조를 적절히 사용하는 연습이 되길 바랍니다!! 
+
 Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
 
 EX1
